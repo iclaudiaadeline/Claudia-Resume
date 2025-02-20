9283c79f52c87d18471139bd17a76b002fc98e32
iclaudiaadeline <a.iradukun3@alustudent.com>
2025-02-20 12:31:10 +0200

    removed my github links

--- js/script.js
+++ js/script.js
@@ -1,34 +1,126 @@
-// Dark Mode Toggle
-const darkModeToggle = document.getElementById("darkModeToggle"); // Get the toggle button
-darkModeToggle.addEventListener("click", () => {
-  // Toggle the dark-mode class on the body
-  document.body.classList.toggle("dark-mode");
-  // Change button text based on current mode
-  darkModeToggle.textContent = document.body.classList.contains("dark-mode")
-    ? "Light Mode"
-    : "Dark Mode";
+document.addEventListener('DOMContentLoaded', function () {
+	document.documentElement.style.setProperty(
+		'--color-light',
+		'#212428'
+	);
+	document.documentElement.style.setProperty(
+		'--color-white',
+		'#212428'
+	);
+	document.documentElement.style.setProperty(
+		'--color-dark',
+		'#212428'
+	);
+	document.documentElement.style.setProperty(
+		'--background-color-1',
+		'#fff'
+	);
+	mode.classList.replace('fa-sun', 'fa-moon');
 });
 
-// Section animation on scroll
-const sections = document.querySelectorAll("section h2");
-// Observer options for Intersection Observer API
-const options = {
-  root: null, // Use viewport as the root
-  threshold: 0.1, // Trigger when 10% of the element is visible
-  rootMargin: "0px", // No margin
-};
+let typed = new Typed('#typed', {
+	strings: [
+		'frontend developer',
+		'backend developer',
+		'mobile developer',
+		'ui/ux designer',
+	],
+	typeSpeed: 50,
+	loop: true,
+});
 
-//Create an Intersection Observer instance
-const observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.style.animationPlayState = "running"; // Start the animation when in view
-      observer.unobserve(entry.target); // Stop observing the element after animation starts
-    }
-  });
-}, options);
+const hamMenu = document.querySelector('.hamMenu');
 
-//obeserve each section header
-sections.forEach((section) => {
-  observer.observe(section);
+hamMenu.addEventListener('click', () => {
+	const navContainer = document.querySelector('.nav_container');
+	const navListOpen = navContainer.getAttribute('navListOpen');
+
+	if (navListOpen === 'false') {
+		document
+			.querySelector('.nav_container')
+			.setAttribute('navListOpen', 'true');
+	} else {
+		document
+			.querySelector('.nav_container')
+			.setAttribute('navListOpen', 'false');
+	}
+
+	if (hamMenu.classList.contains('fa-bars')) {
+		hamMenu.classList.replace('fa-bars', 'fa-xmark');
+	} else {
+		hamMenu.classList.replace('fa-xmark', 'fa-bars');
+	}
+});
+
+const navLinks = document.querySelectorAll('.nav_link');
+
+navLinks.forEach((navLink) => {
+	navLink.addEventListener('click', (e) => {
+		document
+			.querySelector('.nav_container')
+			.setAttribute('navListOpen', 'false');
+
+		hamMenu.classList.replace('fa-xmark', 'fa-bars');
+	});
+});
+
+// DYNAMIC ASCENT COLOR
+
+const colorVar = document.documentElement.getAttribute('--color-blue');
+const colorVaribleHolders = document.querySelectorAll('.varColor');
+
+colorVaribleHolders.forEach((holder) => {
+	holder.addEventListener('click', (e) => {
+		const colorValue = holder.getAttribute('colorValue');
+		document.documentElement.style.setProperty(
+			'--color-blue',
+			colorValue
+		);
+	});
+});
+
+// ========================================
+
+// SWITCHING BETWEEN DARK AND LIGHT MODES
+
+const mode = document.querySelector('.mode');
+
+mode.addEventListener('click', (_) => {
+	if (mode.classList.contains('fa-sun')) {
+		document.documentElement.style.setProperty(
+			'--color-light',
+			'#212428'
+		);
+		document.documentElement.style.setProperty(
+			'--color-white',
+			'#212428'
+		);
+		document.documentElement.style.setProperty(
+			'--color-dark',
+			'#212428'
+		);
+		document.documentElement.style.setProperty(
+			'--background-color-1',
+			'#fff'
+		);
+		mode.classList.replace('fa-sun', 'fa-moon');
+	} else {
+		document.documentElement.style.setProperty(
+			'--color-light',
+			'hsl(0, 0%, 80%)'
+		);
+		document.documentElement.style.setProperty(
+			'--color-white',
+			'#fff'
+		);
+		document.documentElement.style.setProperty(
+			'--color-dark',
+			'#000'
+		);
+		document.documentElement.style.setProperty(
+			'--background-color-1',
+			'#212428'
+		);
+		mode.classList.replace('fa-moon', 'fa-sun');
+	}
 });
